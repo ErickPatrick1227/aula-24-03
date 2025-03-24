519c78fb83814320d8d4cab977a56c54dc224ad3
Aluno FG <alunofg@animaedu.intranet>
2025-03-24 20:41:00 -0300

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var nome = "erick"
+let idade = 19
+const ehEstudante = true
+
+const array = [1,2,3,4]
+
+let objeto = {nome: "seila", idade: 100}
+
+console.log(nome, idade, ehEstudante)
+
+console.log("Tipos das variaves:", typeof idade, typeof objeto, typeof nome, typeof ehEstudante,);
---
+++ index.js
@@ -0,0 +1,19 @@
+var num1 = 2
+
+var num2 = 4
+
+var num3 = 6
+
+var num4 = 4
+
+var num5 = 0
+
+var resul = num1 + num2 * num3 / num4 - num5
+
+console.log(resul)
+
+if (resul <= 5){
+    console.log("reprovado")
+}else{
+    console.log("aprovado")
+}
