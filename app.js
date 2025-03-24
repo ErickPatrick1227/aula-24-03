0a1221c625be0d3b202f6e8c21760705a8846f7f
Aluno FG <alunofg@animaedu.intranet>
2025-03-24 20:45:41 -0300

    pessoa

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const array = [1,2,3,4]
 
 let objeto = {nome: "seila", idade: 100}
 
-console.log(nome, idade, ehEstudante)
+console.log("pessoa:", nome, idade, ehEstudante)
 
 console.log("Tipos das variaves:", typeof idade, typeof objeto, typeof nome, typeof ehEstudante,);
